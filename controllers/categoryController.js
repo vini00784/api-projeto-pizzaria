fcad4128be8d1875828209492f5bf706c13dad71
vini00784 <vo70346@gmail.com>
2022-11-30 10:31:36 -0300

    controller to make CRUD of categories

---
+++ controllers/categoryController.js
@@ -0,0 +1,34 @@
+/*
+    Objetivo:     Arquivo responsável pela manipulação dos dados das categorias dos produtos com o Banco de Dados (insert, update, delete e select)
+    Autores:      Vinícius Santos Oliveira e Arthur Piloto Silva
+    Data Criação: 23/11/2022
+    Versão:       1.0
+*/
+
+// Arquivo de mensagens padronizadas
+const { MESSAGE_ERROR, MESSAGE_SUCCESS } = require('../module/config.js')
+
+// Função que gera novo produto no BD
+const newCategory = async (category) => {
+    
+}
+
+// Função que atualiza produto no BD
+const updateCategory = async (category) => {
+
+}
+
+// Função que delete produto do BD
+const deleteCategory = async (id) => {
+
+}
+
+// Função que lista todos os produtos do BD
+const listAllCategories = async () => {
+    
+}
+
+module.exports = {
+    newCategory,
+    listAllCategories
+}
