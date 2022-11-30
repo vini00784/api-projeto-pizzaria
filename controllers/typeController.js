a088907f5f0eed3ee89db3d5c332459fa610efb9
vini00784 <vo70346@gmail.com>
2022-11-30 10:30:57 -0300

    controller to make CRUD of types

---
+++ controllers/typeController.js
@@ -0,0 +1,29 @@
+/*
+    Objetivo:     Arquivo responsável pela manipulação dos dados dos tipos de produtos com o Banco de Dados (insert, update, delete e select)
+    Autores:      Vinícius Santos Oliveira e Arthur Piloto Silva
+    Data Criação: 23/11/2022
+    Versão:       1.0
+*/
+
+const { MESSAGE_ERROR, MESSAGE_SUCCESS } = require("../module/config")
+
+const newType = async (type) => {
+    
+}
+
+const updateType = async (type) => {
+
+}
+
+const deleteType = async(id) => {
+
+}
+
+const listAllTypes = async () => {
+    
+}
+
+module.exports = {
+    newType,
+    listAllTypes
+}
