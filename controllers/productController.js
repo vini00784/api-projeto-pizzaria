8dad7cc763592c51e0a5ea626324ba932b71a92c
vini00784 <vo70346@gmail.com>
2022-11-30 10:29:16 -0300

    controller to make CRUD of products

---
+++ controllers/productController.js
@@ -0,0 +1,49 @@
+/*
+    Objetivo:     Arquivo responsável pela manipulação dos dados dos alunos com o Banco de Dados (insert, update, delete e select)
+    Autores:      Vinícius Santos Oliveira e Arthur Piloto Silva
+    Data Criação: 23/11/2022
+    Versão:       1.0
+*/
+
+// Arquivo de mensagens padronizadas
+const { MESSAGE_ERROR, MESSAGE_SUCCESS } = require('../module/config.js')
+
+// Função que gera novo produto no BD
+const newProduct = async (product) => {
+
+}
+
+// Função que atualiza produto no BD
+const updateProduct = async () => {
+
+}
+
+// Função que delete produto do BD
+const deleteProduct = async () => {
+
+}
+
+// Função que lista todos os produtos do BD
+const listAllProducts = async () => {
+    
+}
+
+// Função que lista os produtos do BD com base na categoria
+const listProductsByCategory = async () => {
+
+}
+
+// Função que lista os produtos do BD com base no tipo
+const listProductsByType = async () => {
+
+}
+
+// Função que lista os produtos do BD com base no nome
+const listProductByName = async () => {
+
+}
+
+module.exports = {
+    newProduct,
+    listAllProducts
+}
