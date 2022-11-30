2478b3a93854f77aaf844541a2545783ed0a0400
vini00784 <vo70346@gmail.com>
2022-11-30 10:26:53 -0300

    model do make CRUD of types

---
+++ models/DAO/type.js
@@ -0,0 +1,37 @@
+/*
+    Objetivo:     API responsável pela manipulação dos dados dos tipos de produto com o Banco de Dados (INSERT, UPDATE, SELECT, DELETE)
+    Autores:      Vinícius Santos Oliveira e Arthur Piloto Silva
+    Data Criação: 23/11/2022
+    Versão:       1.0
+*/
+
+// Import da classe PrismaClient, que é responsável pelas interações com o Banco de Dados
+const { PrismaClient } = require('@prisma/client')
+
+// Instância da classe PrismaClient
+const prisma = new PrismaClient()
+
+// Função para inserção de um novo tipo
+const insertType = async (type) => {
+    
+}
+
+// Função para atualização de um tipo
+const updateType = async (type) => {
+
+}
+
+// Função para exclusão de um tipo
+const deleteType = async (id) => {
+
+}
+
+// Função para retornar todos os tipos
+const selectAllTypes = async () => {
+    
+}
+
+module.exports = {
+    insertType,
+    selectAllTypes
+}
