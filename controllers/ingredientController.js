d17243432d778527b95b865083577c8d95876c5e
vini00784 <vo70346@gmail.com>
2022-12-01 08:47:58 -0300

    model and controller to make CRUD of ingredients

---
+++ controllers/ingredientController.js
@@ -0,0 +1,36 @@
+/*
+    Objetivo:     Arquivo responsável pela manipulação dos dados dos ingredientes com o Banco de Dados (insert, update, delete e select)
+    Autores:      Vinícius Santos Oliveira e Arthur Piloto Silva
+    Data Criação: 01/12/2022
+    Versão:       1.0
+*/
+
+// Arquivo de mensagens padronizadas
+const { MESSAGE_ERROR, MESSAGE_SUCCESS } = require('../module/config.js')
+
+// Função que gera novo produto no BD
+const newIngredient = async (ingredient) => {
+
+}
+
+// Função que atualiza produto no BD
+const updateIngredient = async (ingredient) => {
+    
+}
+
+// Função que delete produto do BD
+const deleteIngredient = async (id) => {
+    
+}
+
+// Função que lista todos os produtos do BD
+const listAllIngredients = async () => {
+    
+}
+
+module.exports = {
+    newIngredient,
+    updateIngredient,
+    deleteIngredient,
+    listAllIngredients
+}
---
+++ models/DAO/ingredient.js
@@ -0,0 +1,39 @@
+/*
+    Objetivo:     API responsável pela manipulação dos dados dos ingredientes com o Banco de Dados (INSERT, UPDATE, SELECT, DELETE)
+    Autores:      Vinícius Santos Oliveira e Arthur Piloto Silva
+    Data Criação: 01/12/2022
+    Versão:       1.0
+*/
+
+// Import da classe PrismaClient, que é responsável pelas interações com o Banco de Dados
+const { PrismaClient } = require('@prisma/client')
+
+// Instância da classe PrismaClient
+const prisma = new PrismaClient()
+
+// Função para inserção de um novo produto
+const insertIngredient = async (ingredient) => {
+
+}
+
+// Função para atualização de um produto
+const updateProduct = async (ingredient) => {
+    
+}
+
+// Função para exclusão de um produto
+const deleteIngredient = async (id) => {
+    
+}
+
+// Função para retornar todos os produtos
+const selectAllIngredients = async () => {
+    
+}
+
+module.exports = {
+    insertIngredient,
+    updateProduct,
+    deleteIngredient,
+    selectAllIngredients
+}
